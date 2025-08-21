84c0fa91d02994270c972a9ba3cac0b5431a369b
Expert of Nothing <141476327+ExOfNot@users.noreply.github.com>
2025-08-21 18:06:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -57,8 +57,8 @@ with col1:
               "июля", "августа", "сентября", "октября", "ноября", "декабря"]
     contract_month = st.selectbox(
         "Месяц заключения договора:",
-        months,
-        value=datetime.now().month - 1  # текукщий по умолчанию
+        months #,
+        # value=datetime.now().month - 1  # текукщий по умолчанию
     )
     
     # Год
